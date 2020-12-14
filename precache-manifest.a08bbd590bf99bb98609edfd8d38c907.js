b0cbf3b38bf317c5b07caaf26419ccbcf25e949f
tamara96mc <tamara.96mc@gmail.com>
2020-12-14 14:36:28 +0100

    Updates

---
+++ precache-manifest.a08bbd590bf99bb98609edfd8d38c907.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6469b0951cf53fe97247447cd80e1c",
+    "url": "/pixabay/index.html"
+  },
+  {
+    "revision": "c961bdea68c07082ae7d",
+    "url": "/pixabay/static/js/2.9dee5255.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pixabay/static/js/2.9dee5255.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52b6641d0f1b628c6cd2",
+    "url": "/pixabay/static/js/main.a0162d1a.chunk.js"
+  },
+  {
+    "revision": "b523f80364cbf854e16f",
+    "url": "/pixabay/static/js/runtime-main.c356cb4e.js"
+  }
+]);
